2b6e97ddddcaebef2cd6b3a1da931a5308ecc0d9
Nikita Berezovskiy <hemenbgmb@gmail.com>
2021-11-30 20:55:55 +0700

    Posts api method

--- lib/core/service/api/api.dart
+++ lib/core/service/api/api.dart
@@ -1,7 +1,9 @@
 import 'package:wecos_forum/core/service/http_client.dart/http_client.dart';
+import 'package:wecos_forum/core/utils/paginative.dart';
 import 'package:wecos_forum/features/authorization/data/models/token_pair_model.dart';
 import 'package:wecos_forum/features/authorization/data/models/user_model.dart';
 import 'package:wecos_forum/features/authorization/domain/entities/token_pair.dart';
+import 'package:wecos_forum/features/dashboard/data/models/post_model.dart';
 
 class Api {
   HttpClient httpClient;
@@ -40,5 +42,22 @@ class Api {
   Future<UserModel> profile() async {
     final response = await httpClient.get('/profile/me');
     return UserModel.fromJson(response!.data['user']);
+  }
+
+  Future<Paginative<List<PostModel>>> getPosts(
+      [int limit = 10, int offset = 0]) async {
+    final response = await httpClient.get(
+      '/posts',
+      {
+        'limit': limit,
+        'offset': offset,
+      },
+    );
+    return Paginative(
+      response!.data['result']['count'],
+      (response.data['result']['rows'] as List)
+          .map((json) => PostModel.fromJson(json))
+          .toList(),
+    );
   }
 }
