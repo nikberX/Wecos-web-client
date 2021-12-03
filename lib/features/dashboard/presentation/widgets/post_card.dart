6b885863faf3d5f941c962c78bc17f725322e738
Nikita Berezovskiy <hemenbgmb@gmail.com>
2021-12-03 20:44:13 +0700

    Feature show post fix

--- lib/features/dashboard/presentation/widgets/post_card.dart
+++ lib/features/dashboard/presentation/widgets/post_card.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:wecos_forum/core/utils/app_colors.dart';
 import 'package:wecos_forum/features/dashboard/domain/entity/post.dart';
+import 'package:wecos_forum/features/view_post/domain/repositories/viewpost_repository.dart';
+import 'package:wecos_forum/features/view_post/presentation/bloc/viewpost_bloc.dart';
+import 'package:wecos_forum/features/view_post/presentation/post_page.dart';
 
 class PostCard extends StatelessWidget {
   final Post post;
@@ -8,85 +13,99 @@ class PostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(
-        vertical: 20,
-        horizontal: 350,
-      ),
-      width: 450,
-      decoration: BoxDecoration(
-        color: AppColor.cardBackground,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.network(post.imageUrl),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (ctx) => BlocProvider(
+              create: (context) =>
+                  ViewPostBloc(GetIt.I.get<ViewPostRepository>()),
+              child: PostPage(postId: post.id),
+            ),
+            settings: RouteSettings(name: '/post/${post.id}'),
+          ),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 350,
+        ),
+        width: 450,
+        decoration: BoxDecoration(
+          color: AppColor.cardBackground,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(post.imageUrl),
+              Align(
                 alignment: Alignment.bottomCenter,
-                color: Colors.black45,
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            post.title,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  color: Colors.black45,
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              post.title,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
                             ),
                           ),
-                        ),
-                        Text(
-                          post.content,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
+                          Text(
+                            post.content,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          'Author : ${post.authorName}',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const SizedBox(
+                            width: 20,
                           ),
-                        ),
-                        Spacer(),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.comment,
+                          Text(
+                            'Author : ${post.authorName}',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
                           ),
-                        ),
-                        Text(
-                          '12',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 12,
+                          Spacer(),
+                          IconButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (ctx) => PostPage(postId: post.id),
+                                    settings: RouteSettings(
+                                        name: '/post/${post.id}')),
+                              );
+                            },
+                            icon: Icon(
+                              Icons.comment,
+                            ),
                           ),
-                        )
-                      ],
-                    )
-                  ],
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
